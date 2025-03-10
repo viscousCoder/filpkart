192694e65d47c6dc905c1c06f653d9116b05a183
viscousCoder <aman.bisht@celestialsys.com>
2025-03-10 14:26:12 +0530

    Adding files to remove dist

--- backend/dist/routes/admin.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const adminCreate_1 = require("../controller/adminCreate");
-const multer_config_1 = require("../utils/multer.config");
-const router = (0, express_1.Router)();
-// Handle file uploads: `outer_image` (single) and `all_images` (multiple)
-router.post("/create-product", multer_config_1.uploadMiddleware, adminCreate_1.handleCreateProducts);
-exports.default = router;
